d334242c94498ccdbae7753abd98f9870baddeed
tizvic <tiziano.vicentini@tiscali.it>
2019-12-29 23:50:11 +0100

    list of transaction: splitted in short/full data

--- controllers/apiBlockchainController.js
+++ controllers/apiBlockchainController.js
@@ -6,14 +6,41 @@ var bcServer = new stellar.Server('https://horizon-testnet.stellar.org');
 //  953459
 module.exports = (app) => {
 
-    // List of transaction in ledger
-    app.get('/api/bc/transactions/:byLedger', (req, res) => {
+        // List of transaction in ledger (full version)
+        app.get('/api/bc/transactions/long/:ledgerId', (req, res) => {
+
+            // get a list of transactions that occurred in ledger 1400
+            bcServer.transactions()
+                .forLedger(req.params.ledgerId)
+                .call().then((r) => { 
+                    // Get all prperties                    
+                    res.send(r); 
+                });
+    
+        });
+
+    // List of transaction in ledger (short version)
+    app.get('/api/bc/transactions/short/:ledgerId', (req, res) => {
 
         // get a list of transactions that occurred in ledger 1400
         bcServer.transactions()
-            .forLedger(req.params.byLedger)
+            .forLedger(req.params.ledgerId)
             .call().then((r) => { 
-                res.send(r); 
+
+                // Get id and 
+                var output = [];
+                for (let index = 0; index < r.records.length; index++) {
+                    const element = r.records[index];
+                    output.push({
+                        id: element.id, 
+                        feeCharged: element.fee_charged, 
+                        feePayed: element.fee_paid, 
+                        memo: element.memo,
+                        operationCount: element.operation_count
+                    });
+                }
+                
+                res.send(output); 
             });
 
     });
